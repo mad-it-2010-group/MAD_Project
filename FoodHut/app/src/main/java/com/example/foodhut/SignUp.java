a0dafc51663052e1202c1a559d8b498120fd0a62
Chanaka Wickramasinghe <blcchanaka@gmail.com>
2019-08-04 22:33:42 +0530

    Navigation Drawer completed

--- FoodHut/app/src/main/java/com/example/foodhut/SignUp.java
+++ FoodHut/app/src/main/java/com/example/foodhut/SignUp.java
@@ -6,21 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 
     public void navigate(View v) {
-        Intent i = new Intent(this, SignUp.class);
-        startActivity(i);
-    }
-
-    public void navHome(View v) {
-        Intent i = new Intent(this, Main2Activity.class);
+        Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
 }
